18884d37e1d2c89cd36b96bb0e599c1f281d4554
Chib-chan <yana_rutsk@mail.ru>
2025-09-30 23:08:00 +0700

    Add files via upload

---
+++ 2sem7lab/Model/Director.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using MovieRating;
+
+namespace MovieRating.Model
+{
+    /// <summary>
+    /// класс для режиссеров
+    /// </summary>
+    public class Director : INotifyPropertyChanged
+    {
+        private string name;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        private int moviesCount;
+        public int MoviesCount
+        {
+            get => moviesCount;
+            set
+            {
+                if (moviesCount != value)
+                {
+                    moviesCount = value;
+                    OnPropertyChanged(nameof(MoviesCount));
+                }
+            }
+        }
+
+        private double averageRating;
+        public double AverageRating
+        {
+            get => averageRating;
+            set
+            {
+                if (averageRating != value)
+                {
+                    averageRating = value;
+                    OnPropertyChanged(nameof(AverageRating));
+                }
+            }
+        }
+
+        private int startYear;
+        public int StartYear
+        {
+            get => startYear;
+            set
+            {
+                if (startYear != value)
+                {
+                    startYear = value;
+                    OnPropertyChanged(nameof(StartYear));
+                }
+            }
+        }
+
+        private int endYear;
+        public int EndYear
+        {
+            get => endYear;
+            set
+            {
+                if (endYear != value)
+                {
+                    endYear = value;
+                    OnPropertyChanged(nameof(EndYear));
+                }
+            }
+        }
+
+        /// <summary>
+        /// конструктор класса (может понадобиться переделать)
+        /// </summary>
+        /// <param name="Name">Имя</param>
+        /// <param name="MoviesCount">Количество проектов</param>
+        /// <param name="AverageRating">Средний рейтинг всех фильмов</param>
+        /// <param name="StartYear">Начало активности</param>
+        /// <param name="EndYear">Конец активности</param>
+        public Director(string name, int moviesCount, double averageRating, int startYear, int endYear)
+        {
+            Name = name;
+            MoviesCount = moviesCount;
+            AverageRating = averageRating;
+            StartYear = startYear;
+            EndYear = endYear;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ 2sem7lab/Model/Movie.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using MovieRating;
+
+namespace MovieRating.Model
+{
+    /// <summary>
+    /// Класс для фильмов
+    /// </summary>
+    public class Movie : INotifyPropertyChanged
+    {
+        private string title;
+        public string Title
+        {
+            get => title;
+            set
+            {
+                if (title != value)
+                {
+                    title = value;
+                    OnPropertyChanged(nameof(Title));
+                }
+            }
+        }
+
+        private string genre;
+        public string Genre
+        {
+            get => genre;
+            set
+            {
+                if (genre != value)
+                {
+                    genre = value;
+                    OnPropertyChanged(nameof(Genre));
+                }
+            }
+        }
+
+        private int year;
+        public int Year
+        {
+            get => year;
+            set
+            {
+                if (year != value)
+                {
+                    year = value;
+                    OnPropertyChanged(nameof(Year));
+                }
+            }
+        }
+
+        private string directorName;
+        public string DirectorName
+        {
+            get => directorName;
+            set
+            {
+                if (directorName != value)
+                {
+                    directorName = value;
+                    OnPropertyChanged(nameof(DirectorName));
+                }
+            }
+        }
+
+        private double rating;
+        public double Rating
+        {
+            get => rating;
+            set
+            {
+                if (rating != value)
+                {
+                    rating = value;
+                    OnPropertyChanged(nameof(Rating));
+                }
+            }
+        }
+
+        public Movie(string title, string genre, int year, string directorName, double rating)
+        {
+            Title = title;
+            Genre = genre;
+            Year = year;
+            DirectorName = directorName;
+            Rating = rating;
+        }
+
+        /// <summary>
+        /// метод для проверки свойства на изменение
+        /// </summary>
+        /// <param name="propertyName">свойство, которое проверяем на изменения</param>
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
