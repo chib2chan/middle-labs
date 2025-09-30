75bc6fe79bf8a82bd4bb1b9a689ba978ddf591ba
Chib-chan <yana_rutsk@mail.ru>
2025-09-30 23:09:03 +0700

    Add files via upload

---
+++ 2sem7lab/View/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MovieRating.ViewModel;
+
+namespace MovieRating.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+    }
+}
